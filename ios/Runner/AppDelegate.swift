9ca0bb0b812c3d4c866e402e7a92c443c3098405
Bahri Rizaldi <bahri.rizaldi13@gmail.com>
2021-10-17 14:18:55 +0700

    Update iOS configuration

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,17 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    // custom code for getting flavor
+    let controller = self.window.rootViewController as! FlutterViewController
+    let channel = FlutterMethodChannel.init(name: "flavor", binaryMessenger: controller.binaryMessenger)
+    channel.setMethodCallHandler { (call, result) in
+        // Note: this method is invoked on the UI thread
+        // Handle battery messages.
+        let flavor = Bundle.main.infoDictionary!["Flavor"]! as! String
+        result(flavor)
+    }
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }

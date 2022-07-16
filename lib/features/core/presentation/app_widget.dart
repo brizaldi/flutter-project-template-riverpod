7b546c8f16144f681d0737bc6a6655d070e3a483
Bahri Rizaldi <bahri.rizaldi13@gmail.com>
2022-07-16 18:30:20 +0700

    Bump up dependencies version

--- lib/features/core/presentation/app_widget.dart
+++ lib/features/core/presentation/app_widget.dart
@@ -47,6 +47,7 @@ class AppWidget extends HookConsumerWidget {
     final router = ref.watch(routerProvider);
 
     return MaterialApp.router(
+      routeInformationProvider: router.routeInformationProvider,
       routeInformationParser: router.routeInformationParser,
       routerDelegate: router.routerDelegate,
       title: Strings.appName,

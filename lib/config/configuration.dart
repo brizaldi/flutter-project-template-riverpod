14e5d404e88aee19c2ea0afaebd5a4e2553a1ce8
Bahri Rizaldi <bahri.rizaldi13@gmail.com>
2022-11-01 18:34:41 +0700

    Fix url path strategy for web

--- lib/config/configuration.dart
+++ lib/config/configuration.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:logging/logging.dart';
 
--- lib/config/src/env.dart
+++ lib/config/src/env.dart
@@ -18,6 +18,8 @@ abstract class Env {
 
       Themes.initUiOverlayStyle();
 
+      usePathUrlStrategy();
+
       final app = await onCreate();
 
       runApp(
